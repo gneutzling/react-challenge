f11247d08570d06d480484f716691570202ee91c
Gabriel Neutzling <gabrielneutzling@gmail.com>
2016-04-22 17:04:52 -0300

    Adding mobile menu and minor improvements on header.

--- src/assets/js/components/Navigation.js
+++ src/assets/js/components/Navigation.js
@@ -1,10 +1,29 @@
 import React, { Component, PropTypes } from 'react';
 
 class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false
+    };
+  }
+
+  toggleMenu() {
+    let isActive = this.state.isActive;
+
+    this.setState({
+      isActive: !isActive
+    });
+  }
+
   render() {
+
+    let isActive = this.state.isActive ? 'is-active' : '';
+
     return (
       <nav className="main-navigation">
-        <ul>
+        <button onClick={this.toggleMenu.bind(this)}></button>
+        <ul className={isActive}>
           <li><a title="Politics">Politics</a></li>
           <li><a title="Business">Business</a></li>
           <li><a title="Tech">Tech</a></li>
