ab55471b09d9057e25384eebc8f1f5138b365a35
Gabriel Neutzling <gabrielneutzling@gmail.com>
2016-05-01 18:37:05 -0300

    Adding Read More button for each entry post.

--- src/assets/js/components/Entry.js
+++ src/assets/js/components/Entry.js
@@ -34,6 +34,9 @@ class Entry extends React.Component {
           {this.props.data.image &&
             <div className="entry-post__image">
               <Media source={entryImages.teaser} />
+              <div className="entry-post__read-more">
+                <a href="#">Read More</a>
+              </div>
             </div>
           }
           <h1>{this.props.data.title}</h1>
